3fc16d018acded48fb319747636f45c7d2141f17
Dong Liu <doliu@microsoft.com>
2024-02-21 16:23:27 +0800

    add proj

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics;
+
+var watch = new Stopwatch();
+watch.Start();
+var hc = new HttpClient();
+await hc.GetAsync("https://example.org");
+watch.Stop();
+Console.WriteLine(watch.Elapsed);
