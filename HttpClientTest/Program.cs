37ad05cad8aa6ba6e2125fb0a74dbe6984d24023
Dong Liu <doliu@microsoft.com>
2024-02-21 19:22:17 +0800

    Update

---
+++ HttpClientTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Net;
+
+var watch = new Stopwatch();
+watch.Start();
+
+HttpClientHandler hch = new()
+{
+    ServerCertificateCustomValidationCallback = (_, _, _, _) => true
+};
+var hc = new HttpClient(hch);
+
+var uri = Environment.GetEnvironmentVariable("URI");
+if (string.IsNullOrEmpty(uri)) uri = "https://localhost:5001";
+new HttpRequestMessage
+{
+    RequestUri = new Uri(uri),
+    VersionPolicy = HttpVersionPolicy.RequestVersionExact,
+    Version = HttpVersion.Version20,
+};
+await hc.GetAsync(uri);
+watch.Stop();
+Console.WriteLine(watch.Elapsed);
--- Program.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Diagnostics;
-
-var watch = new Stopwatch();
-watch.Start();
-var hc = new HttpClient();
-await hc.GetAsync("https://example.org");
-watch.Stop();
-Console.WriteLine(watch.Elapsed);
